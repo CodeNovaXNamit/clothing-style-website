797beef2ce46d10b6fdce5ce002d821dfc4c9eab
CodeNovaXNamit <namit.profession@gmail.com>
2026-01-31 20:49:58 +0530

    Made some changes but it made it worse

--- wp-content/themes/astra-child02/single-outfit.php
+++ wp-content/themes/astra-child02/single-outfit.php
@@ -1,24 +1,29 @@
-<?php
-get_header();
-the_post();
-?>
+<?php get_header(); ?>
 
 <div class="single-outfit-container">
 
-  <div class="single-outfit-left">
+  <!-- 1️⃣ Image -->
+  <div class="outfit-media">
     <?php the_post_thumbnail('large'); ?>
   </div>
 
-  <div class="single-outfit-right">
-
+  <!-- 2️⃣ Details -->
+  <div class="outfit-info">
     <h1><?php the_title(); ?></h1>
 
     <div class="outfit-description">
       <?php the_content(); ?>
     </div>
+  </div>
 
+  <!-- 3️⃣ Buy buttons -->
+  <div class="outfit-actions">
     <?php render_outfit_buttons(); ?>
+  </div>
 
+  <!-- 4️⃣ Ratings / future -->
+  <div class="outfit-meta">
+    <!-- ratings, tags, season, etc (future) -->
   </div>
 
 </div>
